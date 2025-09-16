dcccbbd17a4e2cb4c913c094a7619d35bae5810c
leodanny0216 <leodannymaria2@gmail.com>
2025-09-16 11:24:04 -0400

    Le Agregue la depndencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,14 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     kotlin("plugin.serialization")
+
 }
+
 
 android {
     namespace = "com.example.registrojugadores"
@@ -84,4 +88,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
