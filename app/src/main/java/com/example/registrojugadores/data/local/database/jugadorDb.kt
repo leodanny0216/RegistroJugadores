e0d7c8f8ffbc39802eed07475200054751145e47
leodanny0216 <leodannymaria2@gmail.com>
2025-09-16 11:26:41 -0400

    Le Agregue la partida en jugadorDb.kt

--- app/src/main/java/com/example/registrojugadores/data/local/database/jugadorDb.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/database/jugadorDb.kt
@@ -2,16 +2,22 @@ package com.example.registrojugadores.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.registrojugadores.data.local.dao.JugadorDao
+import com.example.registrojugadores.data.local.dao.PartidaDao
 import com.example.registrojugadores.data.local.entity.JugadorEntity
+import com.example.registrojugadores.data.local.entity.PartidaEntity
 
 @Database(
     entities = [
-        JugadorEntity::class
+        JugadorEntity::class,
+        PartidaEntity::class
     ],
-    version = 1,
+    version = 5,
     exportSchema = false
 )
-abstract class JugadorDb : RoomDatabase() {
-    abstract fun JugadorDao(): JugadorDao
+@TypeConverters(RoomConverters::class) // Para fechas
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun jugadorDao(): JugadorDao
+    abstract fun partidaDao(): PartidaDao
 }
