f50dda53ded734deb3d36cdd72359dfce4443270
leodanny0216 <leodannymaria2@gmail.com>
2025-09-23 13:24:57 -0400

    Le agregue los logros a jugadorDb.kt

--- app/src/main/java/com/example/registrojugadores/data/local/database/jugadorDb.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/database/jugadorDb.kt
@@ -4,20 +4,24 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.registrojugadores.data.local.dao.JugadorDao
+import com.example.registrojugadores.data.local.dao.LogroDao
 import com.example.registrojugadores.data.local.dao.PartidaDao
 import com.example.registrojugadores.data.local.entity.JugadorEntity
+import com.example.registrojugadores.data.local.entity.LogroEntity
 import com.example.registrojugadores.data.local.entity.PartidaEntity
 
 @Database(
     entities = [
         JugadorEntity::class,
         PartidaEntity::class
+        , LogroEntity::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 @TypeConverters(RoomConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
     abstract fun partidaDao(): PartidaDao
+    abstract fun LogroDao(): LogroDao
 }
