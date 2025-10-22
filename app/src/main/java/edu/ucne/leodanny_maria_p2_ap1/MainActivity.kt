6dfba63c5ae659891440f93955b8b36f5592dc4a
leodanny0216 <leodannymaria2@gmail.com>
2025-10-22 17:57:04 -0400

    asi lo pude hacer y un comimts por el tiempo

---
+++ app/src/main/java/edu/ucne/leodanny_maria_p2_ap1/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.ucne.leodanny_maria_p2_ap1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.leodanny_maria_p2_ap1.ui.theme.Leodanny_maria_P2_AP1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Leodanny_maria_P2_AP1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Leodanny_maria_P2_AP1Theme {
+        Greeting("Android")
+    }
+}
